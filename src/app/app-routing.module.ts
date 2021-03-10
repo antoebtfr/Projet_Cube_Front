2d23035aa573013860a6982fda9e0d32c47b52d1
antoebtfr <antoebt@hotmail.com>
2021-03-10 11:30:06 +0100

    style: log in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 
 const routes: Routes = [
-  {path: '' , component: HomepageComponent}
+  {path: '' , component: HomepageComponent},
+  { path: 'auth', loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule) }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './pages/homepage/homepage.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomepageCommentsComponent } from './components/homepage-comments/homepage-comments.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { SearchbarComponent } from './components/searchbar/searchbar.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +14,8 @@ import { SearchbarComponent } from './components/searchbar/searchbar.component';
     HomepageComponent,
     NavbarComponent,
     HomepageCommentsComponent,
-    SearchbarComponent
+    SearchbarComponent,
+
   ],
   imports: [
     BrowserModule,
