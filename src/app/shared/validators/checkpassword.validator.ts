baf052640ed526072375ef1d044f9b563a5cb1d7
antoebtfr <antoebt@hotmail.com>
2021-03-10 15:29:14 +0100

    style : title + favicon,

--- src/app/shared/validators/checkpassword.validator.ts
+++ src/app/shared/validators/checkpassword.validator.ts
@@ -1,6 +1,8 @@
 import { FormGroup } from "@angular/forms";
 
 export function checkPasswords(group: FormGroup): boolean {
+        group.get('').valueChanges
+
         const password = group.get('password').value;
         const confirmPassword = group.get('confirmPassword').value;
 
