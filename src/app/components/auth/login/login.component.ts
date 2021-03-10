8e1ff9ec90cef0ff02cd1fc10d67b831f716d37b
antoebtfr <antoebt@hotmail.com>
2021-03-10 12:03:58 +0100

    feat: log in form

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EmailValidator, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+  ) { }
+
+  public submitted = false; 
+
+  public logInForm = this.fb.group({
+    email : ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  }
+  );
 
   ngOnInit(): void {
   }
 
+  public onSubmit(){
+    this.submitted = true;
+    console.log(this.logInForm);
+    
+  }
 }
