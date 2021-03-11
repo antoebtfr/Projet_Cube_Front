a798e3760d98e22e2cb49ad9f33bf768be95a86e
antoebtfr <antoebt@hotmail.com>
2021-03-11 09:26:52 +0100

    style: responsive log in

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
-  public modal = 'signup';
+  public modal = 'login';
   constructor() { }
 
   ngOnInit(): void {
