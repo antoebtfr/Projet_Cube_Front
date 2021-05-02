734e61201d766e2721ef74b0f7ac742c2280ca32
antoebtfr <antoebt@hotmail.com>
2021-05-02 19:46:30 +0200

    stats

--- src/app/admin/adminstats/adminstats.component.ts
+++ src/app/admin/adminstats/adminstats.component.ts
@@ -10,6 +10,9 @@ export class AdminstatsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    console.log($.getJSON('https://api.db-ip.com/v2/free/self', function(data) {
+      console.log(JSON.stringify(data, null, 2));
+    }))
   }
 
 }
